e93d31435a13cbeb9f2d7aaad8247c0317e8c239
Corentin Moussard <hello@alune.fr>
2017-08-22 19:52:49 +0200

    commit existing code from local

---
+++ _site/assets/main.js
@@ -0,0 +1,41 @@
+$(window).on('scroll', function() {
+    var scrollPosition = window.pageYOffset;
+    var scopeIfMultiple = (scrollPosition % 600);
+    var triggerOne = 100;
+    var triggerTwo = 200;
+    var triggerThree = 300;
+    var triggerFour = 400;
+    var triggerFive = 500;
+
+    // console.log(scopeIfMultiple);
+
+    if(scopeIfMultiple < triggerOne) {
+        $(".logo").css('display', 'none');
+        $(".logo-one").css('display', 'block');
+    }
+
+    if((scopeIfMultiple > triggerOne) && (scopeIfMultiple < triggerTwo)) {
+        $(".logo").css('display', 'none');
+        $(".logo-two").css('display', 'block');
+    }
+
+    if((scopeIfMultiple > triggerTwo) && (scopeIfMultiple < triggerThree)) {
+        $(".logo").css('display', 'none');
+        $(".logo-three").css('display', 'block');
+    }
+
+    if((scopeIfMultiple > triggerThree) && (scopeIfMultiple < triggerFour)) {
+        $(".logo").css('display', 'none');
+        $(".logo-four").css('display', 'block');
+    }
+
+    if((scopeIfMultiple > triggerFour) && (scopeIfMultiple < triggerFive)) {
+        $(".logo").css('display', 'none');
+        $(".logo-five").css('display', 'block');
+    }
+
+    if(scopeIfMultiple > triggerFive) {
+        $(".logo").css('display', 'none');
+        $(".logo-six").css('display', 'block');
+    }
+});
