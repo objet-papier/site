5cc476dc571330da806ed742845361efcc1094c3
Corentin Moussard <cmoussard@deolan.com>
2017-09-18 15:05:59 +0200

    finished project page YEAY

--- _op-release/op001-objet-papier.md
+++ _op-release/op001-objet-papier.md
@@ -2,7 +2,7 @@
 title: Op001 Objet Papier
 layout: project
 number: OP-001
-auteur: Ronan Deshaie
+auteur: Ronan Deshaies
 name: Objet Papier
 cover-image: op001-cover
 
@@ -18,9 +18,10 @@ images:
   - op001-2.jpg
   - op001-3.jpg
   - op001-4.jpg
+  - op001-5.jpg
 ---
 
 
-«&thinsp;Objet Papier&thinsp;» constitue à la fois la conclusion et la synthèse de la trilogie initiée par les pièces « Rejet/Rejection » et « Et les mouettes… »
+«&thinsp;Objet Papier&thinsp;» constitue à la fois la conclusion et la synthèse de la trilogie initiée par les pièces «&thinsp;Rejet&thinsp;/&thinsp;Rejection&thinsp;» et «&thinsp;Et les mouettes…&thinsp;»
 Tandis que la première création vidéo avait pour sujets l'intégration en milieu urbain, l'adaptation à un environnement étranger et la difficulté à faire corps avec une communauté donnée, la seconde abordait la réappropriation et la reconstruction psychologique dans un Paris post-attentats.
 Conséquence directe d'un projet de magazine avorté, «&thinsp;Objet&nbsp;Papier&thinsp;» est le fruit d'un travail commun portant sur les obsessions toxiques, interrogeant la cohérence de la presse papier et revisitant les thématiques précédentes.
--- assets/project.js
+++ assets/project.js
@@ -1,13 +1,53 @@
 $(window).on('scroll', function() {
-  var scrollPosition = window.pageYOffset;
+  var projectScrollPosition = window.pageYOffset;
   var projectHeight = $('.project-page-content').height();
   var totalPrHeight = (projectHeight - 1);
 
-  if(scrollPosition > totalPrHeight) {
+  if(projectScrollPosition > totalPrHeight) {
       $('.project-description-container').addClass('visible');
+      $('.back-home-link').addClass('white');
   }
 
-  if(scrollPosition < totalPrHeight) {
+  if(projectScrollPosition < totalPrHeight) {
       $('.project-description-container').removeClass('visible');
+      $('.back-home-link').removeClass('white');
   }
+});
+
+$(document).ready(function(){
+
+    $('.project-image').each(function(){
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        var rollDice = getRandomInt(-20, 20)
+
+        $(this).css('transform', 'rotate(' + rollDice + 'deg)');
+    });
+
+    $('.buy-link').each(function(){
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        var rollDice = getRandomInt(-30, 30)
+
+        $(this).css('transform', 'rotate(' + rollDice + 'deg)');
+    });
+
+    var projectScrollPosition = window.pageYOffset;
+    var projectHeight = $('.project-page-content').height();
+    var totalPrHeight = (projectHeight - 1);
+
+    if(projectScrollPosition > totalPrHeight) {
+        $('.project-description-container').addClass('visible');
+        $('.back-home-link').addClass('white');
+    }
+
+    if(projectScrollPosition < totalPrHeight) {
+        $('.project-description-container').removeClass('visible');
+        $('.back-home-link').removeClass('white');
+    }
+
 });
