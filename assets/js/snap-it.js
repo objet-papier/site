e95fd721efb02d39ff3aa39df907593421548479
AluneCM <hello@alune.fr>
2019-10-28 19:26:42 +0100

    add font test to snap it website

--- assets/js/snap-it.js
+++ assets/js/snap-it.js
@@ -73,3 +73,30 @@ var printerAnimData = {
 var printerAnim = bodymovin.loadAnimation(printerAnimData);
 
 printerAnim.setSubframe(false)
+
+
+
+// Editable text
+
+const editableSectionWrapper = document.querySelector(".test-section");
+const editableSection = document.querySelector("#editable-section");
+
+const fontSizeDial = document.querySelector("#font-size-dial");
+const letterSpacingDial = document.querySelector("#letter-spacing-dial");
+const lineHeightDial = document.querySelector("#line-height-dial");
+
+editableSectionWrapper.addEventListener("click", function() {
+  editableSection.focus();
+});
+
+fontSizeDial.addEventListener("input", function(){
+  editableSection.style.fontSize = fontSizeDial.value + "px";
+})
+
+letterSpacingDial.addEventListener("input", function(){
+  editableSection.style.letterSpacing = letterSpacingDial.value + "px";
+})
+
+lineHeightDial.addEventListener("input", function(){
+  editableSection.style.lineHeight = lineHeightDial.value;
+})
